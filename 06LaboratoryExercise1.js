7bda37fe41da72cb5f7ad0ec129f3773cc585ab9
AKCcoding <alraeatwork@gmail.com>
2023-05-19 21:36:39 +0800

    Installed npm packages and created a new file

---
+++ 06LaboratoryExercise1.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')({sigint: true});
+var favoriteArtistAge = prompt("What is the age of your favorite artist?");
+compareArtistAge(favoriteArtistAge);
+
+function compareArtistAge(artistAge) {
+    if (artistAge < 25) {
+      console.log("They're " + artistAge + " years old and they're younger than Google.");
+    } else if (artistAge == 25) {
+      console.log("They're " + artistAge + " years old and they're as old as Google.");
+    } else {
+      console.log("They're " + artistAge + " years old and they're older than Google.");
+    }
+  }
